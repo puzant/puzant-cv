6140a99753a2191362f5227a264efcb5336b13b2
Puzant <PBAKJEJIAN@hotmail.com>
2024-01-15 09:35:00 +0200

    🔥 remove mock data

--- main.js
+++ main.js
@@ -23,7 +23,6 @@ export default function app() {
           },
           { projects: [], experiences: [], education: [] },
         );
-        console.log('🚀 ~ Storyblok.get ~ groupedStories;:', groupedStories);
 
         this.content = groupedStories;
       });
@@ -65,45 +64,6 @@ export default function app() {
       'NodeJs',
       'Express',
       'MySQL',
-    ],
-    projects: [
-      {
-        name: 'Movie Tracker',
-        link: 'https://react-app-movies-tracker.netlify.app/',
-        description:
-          'Simple application that allows users to track and manage their favorite movies, explore movie details.',
-        src: './project-1.png',
-        tech: ['React', 'React Query', 'React Router', 'TypeScript', 'TailwindCSS'],
-      },
-      {
-        name: 'LOCUS',
-        link: 'https://www.locus.eu/',
-        description: 'Real Estate management platform',
-        src: './project-4.png',
-        tech: ['React', 'React Query', 'React Router', 'Formik', 'Axios', 'TailwindCSS'],
-      },
-      {
-        name: 'Password Generator',
-        link: 'https://complex-password-generator-app.netlify.app/',
-        description:
-          'generate passwords with various customization options, including capital letters, numbers, symbols,& pronounceable.',
-        src: './project-2.jpeg',
-        tech: ['React', 'TailwindCSS'],
-      },
-      {
-        name: 'Ethtronomy',
-        link: 'https://ethtronomy.com/',
-        description: '',
-        src: './project-3.jpeg',
-        tech: ['React', 'React Router', 'CSS'],
-      },
-    ],
-    navLinks: [
-      { name: 'About', ref: '#about' },
-      { name: 'Experience', ref: '#experience' },
-      { name: 'Education', ref: '#education' },
-      { name: 'Skills', ref: '#skills' },
-      { name: 'Portfolio', ref: '#portfolio' },
     ],
   };
 }
